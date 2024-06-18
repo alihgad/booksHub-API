b55950125a5f1d448fc278663dc1a03f6c54a627
ali <alihgad2@gmail.com>
2024-06-18 13:47:47 +0300

    add

--- src/app/DB/connection.js
+++ src/app/DB/connection.js
@@ -1,10 +1,13 @@
+import { configDotenv } from "dotenv";
 import mongoose from "mongoose";
 
 const connectionDB = async () => {
- return await mongoose.connect("mongodb+srv://alihgad2:w3rlw2u4YMO785xE@bookshub.4uutypb.mongodb.net/assignment-8")
+    let url = configDotenv().parsed.URL
+ return await mongoose.connect(url)
 .then(()=>{
     console.log("3ash")
 }).catch(err => console.error(err));
 }
 
 export default connectionDB
+
